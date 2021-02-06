af23251beebc1b226187950f8c3e605dac9927ea
Tulio <tuliolopesr@gmail.com>
2021-02-06 07:26:48 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/assets/styles/components/Navbar';
 
 const App = () => {
-    return <h1>Hello App</h1>
+    return (
+        <Navbar/>
+    );
 }
 
 export default App;
